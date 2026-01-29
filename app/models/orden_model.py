3520714f90d4280aac37c4dc213c13f8b2b2d99c
Neftis666 <120331018+Neftis666@users.noreply.github.com>
2026-01-28 20:04:15 -0500

    Update orden_model.py

--- app/models/orden_model.py
+++ app/models/orden_model.py
@@ -3,13 +3,21 @@ from datetime import datetime
 
 class OrdenCompra(db.Model):
     __tablename__ = 'ordenes_compra'
-
+    
     id = db.Column(db.Integer, primary_key=True)
     numero_orden = db.Column(db.String(50), unique=True, nullable=False)
     fecha = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    # NUEVOS CAMPOS
+    fecha_emision = db.Column(db.Date, nullable=True)
+    numero_orden_cliente = db.Column(db.String(100), nullable=True)
+    sucursal_cliente = db.Column(db.String(200), nullable=True)
+    
+    # Campos existentes (proveedor ahora es conceptualmente "cliente")
     proveedor = db.Column(db.String(200), nullable=False)
     direccion_proveedor = db.Column(db.String(200), nullable=True)
     telefono_proveedor = db.Column(db.String(20), nullable=True)
+    
     subtotal = db.Column(db.Float, nullable=False)
     iva = db.Column(db.Float, nullable=False)
     descuento = db.Column(db.Float, default=0)
@@ -20,14 +28,13 @@ class OrdenCompra(db.Model):
     observaciones = db.Column(db.Text, nullable=True)
     
     detalles = db.relationship('DetalleOrden', backref='orden', lazy=True, cascade='all, delete-orphan')
-
+    
     def __repr__(self):
         return f'<OrdenCompra {self.numero_orden}>'
 
-
 class DetalleOrden(db.Model):
     __tablename__ = 'detalles_orden'
-
+    
     id = db.Column(db.Integer, primary_key=True)
     orden_id = db.Column(db.Integer, db.ForeignKey('ordenes_compra.id'), nullable=False)
     producto_descripcion = db.Column(db.String(200), nullable=False)
@@ -35,7 +42,7 @@ class DetalleOrden(db.Model):
     unidad_medida = db.Column(db.String(20), default='UND')
     precio_unitario = db.Column(db.Float, nullable=False)
     subtotal = db.Column(db.Float, nullable=False)
-
+    
     def __repr__(self):
         return f'<DetalleOrden {self.orden_id}>'
 
